b9d006d5fa49b3ca8f2adfa98e36464c263f9260
yarentamir <yarden44441@gmail.com>
2021-11-09 15:43:55 +0200

    comit

--- 6.4/6.4.js
+++ 6.4/6.4.js
@@ -2,10 +2,17 @@
   "use strict";
   const populations = [1439323776, 331002651, 46754778, 32971854];
 
+  function percentageOfWorld(population) {
+    const worldPopulation = 7900000000;
+    return ((population * 100) / worldPopulation).toFixed(2);
+  }
+
   const populationPercentages = () => {
     const percentages = [];
-    for (const i of populations) {
-      
+    for (let i = 0; i < populations.length; i++) {
+      percentages[i] = percentageOfWorld(populations[i]);
     }
+    return percentages;
   };
+  console.log(populationPercentages());
 })();
