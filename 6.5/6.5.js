19fb724323d274736dbf4ad52a6d171d905ae755
yarentamir <yarden44441@gmail.com>
2021-11-09 17:52:46 +0200

    6.5

---
+++ 6.5/6.5.js
@@ -0,0 +1,15 @@
+(function () {
+  "use strict";
+  const filledArray = new Array(100).fill("hello");
+  const countingArr = Array.from({ length: 100 }, (_, i) => i + 1);
+  const someObj = { 0: "hello", 1: "world" };
+  const objectToArrOnlyValues = Object.values(someObj);
+  const arrToObj = Object.assign({}, ["a", "b", "c"]);
+  const isArray = Array.isArray(objectToArrOnlyValues);
+  const copyArrWontEffect = [...objectToArrOnlyValues];
+  const copyArrEffect = objectToArrOnlyValues;
+
+  copyArrWontEffect.push("he");
+  copyArrEffect.push("bhe");
+  console.log(objectToArrOnlyValues);
+})();
