15a205f5d879802b1afa10403d58a7bd6954af30
yarentamir <yarden44441@gmail.com>
2021-11-09 13:13:53 +0200

    5.3

--- 5.3/5.3.js
+++ 5.3/5.3.js
@@ -1,3 +1,27 @@
 (function () {
   "use strict";
+  function passwordValidation(psw) {
+    return psw.length > 7 ? "strong" : "weak";
+  }
+
+  function passwordValidation2(psw) {
+    if (psw.length > 7) return "strong";
+    else return "weak";
+  }
+
+  function passwordValidation3(psw) {
+    if (psw.length > 7 && psw.length != 0) return "strong";
+    else return "weak";
+  }
+
+  function advancedPasswordValidation(psw) {
+    const firstTrinary = psw.length > 7 ? "Strong" : "Weak";
+    const secondTrinary =
+      psw.length > 7 && /[A-Z]/.test(psw) ? "Very Strong" : firstTrinary;
+    return secondTrinary;
+  }
+
+  console.log(advancedPasswordValidation("psffffffffffW")); // very strong
+  console.log(advancedPasswordValidation("psffffffffff")); // strong
+  console.log(advancedPasswordValidation("psf")); // weak
 })();
